1a53df37013ca32ed0c8b16fbb84dd7c62f48060
Gaurav C <cggaurav@gmail.com>
2023-12-24 16:38:20 +0530

    Vercel

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+const express = require('express');
+const http = require('http')
 const Twitter = require('twitter')
 const fs = require('fs')
-const got = require('got')
+const got = require('got');
+
+let app = express();
+app.set('port', process.env.PORT || 3000)
+
+const SHEET_ID = process.env.SHEET_ID
+const SHEET_URL = `https://opensheet.vercel.app/${SHEET_ID}/1`
 
 const twitter = new Twitter({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -11,20 +19,38 @@ const twitter = new Twitter({
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
-const SHEET_ID = process.env.SHEET_ID
-const SHEET_URL = `https://opensheet.vercel.app/${SHEET_ID}/1`
+app.get('/api', (req, res) => {
+	got(SHEET_URL, { json: true })
+		.then(response => {
+			let reminders = response.body
+			// console.log(`We have ${reminders.length} reminders`)
+			let reminder = reminders[Math.floor(Math.random() * reminders.length) + 1]
 
-got(SHEET_URL, { json: true })
-.then(response => {
-	let reminders = response.body
-	// console.log(`We have ${reminders.length} reminders`)
-	let reminder = reminders[Math.floor(Math.random() * reminders.length) + 1]
+			console.log(`Tweeting .. "${reminder['QUOTE']} ${reminder['WHO']}"`)
 
-	console.log(`Tweeting .. "${reminder['QUOTE']} ${reminder['WHO']}"`)
+			twitter.post('statuses/update', { status: [reminder['QUOTE'].trim(), reminder['WHO'].trim(), '#day', "#reminder"].join(' ') })
+				.then((s) => {
+					console.log(`Tweeting .. `, s)
+					res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+				})
+				.catch((e) => {
+					console.log('We have an error', e)
+					res.end(`Err ${e}`);
+				})
 
-	twitter.post('statuses/update', { status: [reminder['QUOTE'].trim(), reminder['WHO'].trim(), '#day', "#reminder"].join(' ') } )
-})
-.catch((err) => {
-	console.err(err)
-	throw err
+		})
+		.catch((err) => {
+			console.log(err)
+			// throw err
+			res.end(`Err ${err}`);
+		})
+	
+});
+
+app.get('/', (req, res) => {
+	res.end('Reminder of the day!')
+});
+
+http.createServer(app).listen(app.get('port'), () => {
+	console.log('Express server listening on port ' + app.get('port'))
 })
