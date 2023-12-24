341974eef9c32a6ae275d3a968eb2568d1f2a5fb
Gaurav C <cggaurav@gmail.com>
2023-12-24 16:52:33 +0530

    Bearer token

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ const SHEET_URL = `https://opensheet.vercel.app/${SHEET_ID}/1`
 const twitter = new Twitter({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.TWITTER_TOKEN_KEY,
-	access_token_secret: process.env.TWITTER_TOKEN_SECRET
+	bearer_token: process.env.TWITTER_BEARER_TOKEN
 })
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
